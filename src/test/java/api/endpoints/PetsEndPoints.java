580a83af32ad79956c6fa38f8a6b8f48567633df
gauravneha <gauravneha1909@gmail.com>
2024-07-01 14:16:55 +1200

    updated endpoints

--- src/test/java/api/endpoints/PetsEndPoints.java
+++ src/test/java/api/endpoints/PetsEndPoints.java
@@ -3,10 +3,15 @@ package api.endpoints;
 import static io.restassured.RestAssured.*;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import api.payload.Category;
+import api.payload.Pets;
+import api.payload.Tag;
 import io.restassured.response.Response;
 
 import io.restassured.matcher.*;
@@ -14,72 +19,162 @@ import io.restassured.matcher.*;
 
 public class PetsEndPoints {
 	
-	@Test(priority=1)
-	void  getpetinfo()
+
+	public static Response  getpetinfo(int petid)
 	
 	{
 		Response response=given()
 		.header("accept","application/json")
-		.pathParam("petid", "1")
+		.pathParam("petid", petid)
 		
 		.when()
 		.get(Routes.get_petbyid);
 		
-		//return  response;
+		return  response;
 
 		
 	}
 
 	
-	@Test(priority=2)
-	void deletepet()
+	public static Response deletepet(int petid)
 	
 	{
 		Response response=given()
 		.header("accept","application/json")
-		.pathParam("petid", "1")
+		.pathParam("petid", petid)
 		
 		.when()
 		.get(Routes.delete_pet);
 		
-		//return  response;
+		return  response;
 
 	}
 		
-		@Test(priority=3)
+
 	
-void uploadpetimage()
+	public static Response uploadpetimage(String filepath, int petid)
 	
 	{
 		
 	
-	File myfile=new File("/Users/gauravkumar/dog.jpg");
+	File myfile=new File(filepath);
 	
 
 	
 	
-		//Response response=
+		Response response=
 				
 				given()
 		.header("accept","application/json")
 		.contentType("multipart/form-data")
 		.multiPart("file", myfile) // Specify file parameter name, file object, and content type
-		.pathParam("petid", "1")
+		.pathParam("petid", petid)
 		
 		.when()
 		.post(Routes.post_upload_image)
-		.then()
-		.statusCode(200)
-		.log().all()
 		;
 		
-		//return  response;
+		return  response;
 
 		
 	}
 
+
+		
+	public static Response addnewpet(Pets petdata, Tag tagdata, Category catdata)
+
+			
+			{
+				Response response=
+						
+				given()
+				.header("accept","application/json")
+				.contentType("application/json")
+				.body(petdata)
+				.body(tagdata)
+				.body(catdata)
+				
+
+				.when()
+				.post(Routes.post_add_pet)
+				;
+				
+				return  response;
+
+				
+			}
 	
 	
+	public static Response getpetsbystatus(String status)
+	
+	{
+		
+		Response response=given()
+				
+			.accept("application/json")	
+			.queryParam("status", status)
+		
+		.when()
+		.get(Routes.get_petbyStatus);
+		
+		
+		return response;
+		
+		
+		
+		
+		
+	}
 	
 	
+	public static Response updatepet(Pets petdata, Tag tagdata, Category catdata)
+
+
+	
+	{
+		Response response=
+				
+		given()
+		.header("accept","application/json")
+		.contentType("application/json")
+		.body(petdata)
+		.body(tagdata)
+		.body(catdata)
+		
+
+		.when()
+		.put(Routes.put_update_pet)
+		;
+		
+		return  response;
+
+		
+	}
+
+
+	public static Response updatepetdata(int petid, String status, String name)
+	
+	{
+		
+		 Map<String, Object> formdata = new HashMap<>();
+		 formdata.put("name", name);
+		 formdata.put("status", status);
+		
+		Response response=given()
+		
+		
+		.when()
+		.header("accept","application/json")
+		.contentType("application/x-www-form-urlencoded")
+		.pathParam("petid", petid)
+		.formParams(formdata)
+		
+		.post(Routes.post_petupdatewithformdata);
+		
+		return response;
+		
+		
+	}
+
+
 }
--- src/test/java/api/payload/Pets.java
+++ src/test/java/api/payload/Pets.java
@@ -1,5 +1,43 @@
 package api.payload;
 
 public class Pets {
+	
+	public int id;
+	public String name; 
+	public String Status;
+	public String photoUrls;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status;
+	}
+	public String getPhotoUrls() {
+		return photoUrls;
+	}
+	public void setPhotoUrls(String photoUrls) {
+		this.photoUrls = photoUrls;
+	}
 
-}
+	
+
+
+	
+	
+	}
+
+
+
